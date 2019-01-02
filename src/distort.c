80ec109108321d99dc4b2dbbb69c8120e1d9e5cd
Greg Wuller <greg@afofo.com>
2019-01-01 19:59:18 -0800

    distort, basic ^2 shaper

--- src/distort.c
+++ src/distort.c
@@ -13,6 +13,10 @@ static inline fix16_t fix16_hi(fix16_t v) {
     return 0xFFFF0000 & v;
 }
 
+static inline fix16_t fix16_clamp(fix16_t v, fix16_t max) {
+    return v > max ? max : v;
+}
+
 void distort_init(distort_t *d) {
     // TODO: move this to be constant static values
     static int initialized = 0;
@@ -20,8 +24,8 @@ void distort_init(distort_t *d) {
         fix16_half = fix16_from_float(0.5);
         fix16_neg_one = fix16_from_int(-1);
 
-        shape_sf = fix16_from_int(128);
-        shape_rmax = fix16_div(fix16_from_int(128 - 1), fix16_two);
+        shape_sf = fix16_from_int(32);
+        shape_rmax = fix16_mul(shape_sf, shape_sf);
 
         initialized = 1;
     }
@@ -70,21 +74,29 @@ fix16_t distort_shape(distort_t *d, fix16_t in) {
         // inverse exp...
         b = fix16_mul(d->param.bend, fix16_neg_one);
         e = fix16_mul(fix16_sub(fix16_one, in), shape_sf);
+        e = fix16_div(fix16_mul(e, e), shape_rmax);
+        e = fix16_sub(fix16_one, e);
+        // printf(" inverse b, e: 0x%.8x, 0x%.8x\n", b, e);
     } else {
         // straight exp...
         b = d->param.bend;
         e = fix16_mul(in, shape_sf);
+        e = fix16_div(fix16_mul(e, e), shape_rmax);
+        // printf("straight b, e: 0x%.8x, 0x%.8x\n", b, e);
     }
 
-    // ^2, scale down to [0,1], then interp...
-    e = fix16_div(fix16_mul(e, e), shape_rmax);
-    v = fix16_lerp16(in, e, b);
+    if (b == 0) {
+        // printf("       use in: 0x%.8x, 0x%.8x\n", in, e);
+        v = in;
+    } else if (b == fix16_one) {
+        // printf("        use e: 0x%.8x, 0x%.8x\n", in, e);
+        v = e;
+    } else {
+        // printf("         lerp: 0x%.8x, 0x%.8x\n", in, e);
+        v = fix16_lerp16(in, e, b);
+    }
+    // printf("            v: 0x%.8x\n", v);
 
     // comp is multiply the output value and clamp the output
-    s = fix16_mul(v, d->param.compress);
-    if (s > fix16_one) {
-        s = fix16_one;
-    }
-
-    return s;
+    return fix16_clamp(fix16_mul(v, d->param.compress), fix16_one);
 }
